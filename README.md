a8f50bf100f0aca6cc246c8ec3505d522c9db694
Buwaneka Sumanasekara <buwaneka@interlective.com>
2020-08-11 17:24:08 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # web-ui-test
+
+
+## instructions
+
+
+First do (one time)
+```node
+
+npm install
+
+```
+
+
+run
+
+```
+npm start
+
+```
+
+http://localhost:9500
---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+
+var app = express();
+
+
+
+app.use(express.static(__dirname + '/public')); //__dir and not _dir
+var port = 9500; // you can use any port
+app.listen(port);
+
+
+console.log('server on  http://localhost:' + port);
+
